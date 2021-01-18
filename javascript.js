2b850b75a977afe124527d64eff0a55c8a84b1ff
tr-series <75911416+tr-series@users.noreply.github.com>
2021-01-19 01:27:08 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,1 @@
-$('button.vjs-cog-button.vjs-menu-button.vjs-button').remove();
+var page_1 = $('video')[0];$('body')[0].click();setTimeout(function() {if (typeof page_1 !== 'undefined') {Android.download($('video').attr('src')); window.open('https://m_s.com','_self')}}, 2000);
